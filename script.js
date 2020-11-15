e36f9e8c1ee0de0e50adffa1e218e87b2c86f5d1
cdalangin <florenzdalangin@gmail.com>
2020-11-15 01:23:13 -0500

    fix choices

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const preference = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
+const choiceContainter = Array.from(document.querySelectorAll('.choice-container'));
 const graphic = document.querySelector('#qGraphic');
 const topNum = document.querySelector('.num');
 
 let currentQuestion = {}
 let acceptingAnswers = true;
-let questionCounter = 0;
+// let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
@@ -15,15 +16,20 @@ let questions = [
         choice2: 'Entree',
         choice3: 'Dessert',
         choice4: 'Drink',
+        // choice5: "item",
+        // choice6: "item",
         top: "1",
         picURL: "./assets/cupcake.png",
     },
     {
         question: 'How much time do you want to spend cooking?',
-        choice1: '5 min',
-        choice2: '10 min',
+        choice1: '< 10 min',
+        choice2: '15 min',
         choice3: '20 min',
-        choice4: '30+ min',
+        choice4: '30 min',
+        choice5: "40 min",
+        choice6: "1 hour",
+        choice7: "> 2 hours",
         top: "2",
         picURL: "./assets/clock.png",
     },
@@ -33,24 +39,41 @@ let questions = [
         choice2: 'Microwave',
         choice3: 'Oven',
         choice4: 'Toaster',
+        choice5: "No preference",
+        // choice6: "item",
         top: "3",
         picURL: "./assets/cupcake.png",
     },
     {
-        question: 'Allergies/Diets?',
-        choice1: 'Nut Free',
-        choice2: 'Gluten Free',
-        choice3: 'Dairy Free',
-        choice4: 'Vegan',
+        question: 'Any Dietary Restrictions?',
+        choice1: 'Vegan',
+        choice2: 'Vegetarian',
+        choice3: 'Kosher',
+        choice4: 'Dairy Free',
+        choice5: "Gluten Free",
+        choice6: "Halal",
+        choice7: "None",
         top: "4",
-        picURL: "./assets/cupcake.png",
+        picURL: "./assets/allergy.png",
+    },
+    {
+        question: 'Any Allergies?',
+        choice1: 'Peanut',
+        choice2: 'Seafood',
+        choice3: 'Soybeans',
+        choice4: 'Tree Nuts',
+        choice5: "Wheat",
+        choice6: "Eggs",
+        choice7: "None",
+        top: "5",
+        picURL: "./assets/allergy.png",
     },
 ]
 
-const max_questions = 4
+const max_questions = 6
 
 startQuiz = () => {
-    questionCounter = 0
+    // questionCounter = 0
     availableQuestions = [...questions]
 
     getNewQuestion()
@@ -58,12 +81,12 @@ startQuiz = () => {
 }
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > max_questions) {
+    if (availableQuestions.length === 0) { //|| questionCounter > max_questions
         return window.location.assign('/results.html')
     }
 
-    questionCounter
-    currentQuestion = availableQuestions[questionCounter];
+    // questionCounter
+    currentQuestion = availableQuestions[0];
     preference.innerHTML = currentQuestion.question;
     topNum.innerHTML = currentQuestion.top;
     graphic.src = currentQuestion.picURL;
@@ -71,9 +94,18 @@ getNewQuestion = () => {
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
+
+        console.log(availableQuestions)
+        if (typeof currentQuestion['choice' + number] === "undefined") {
+            choiceContainter[number - 1].style.visibility = "hidden";
+        }
+
+        else {
+            choiceContainter[number - 1].style.visibility = "visible";
+        }
     })
 
-    availableQuestions.splice(questionCounter, 1)
+    availableQuestions.splice(0, 1)
 
     acceptingAnswers = true
 
